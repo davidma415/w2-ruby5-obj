4109e09457917ebd62a7eb43ee84364decd07850
David Ma <davidma415@gmail.com>
2018-07-19 15:51:49 -0400

    Finished exercise 1

--- exercise1-obj.rb
+++ exercise1-obj.rb
@@ -15,8 +15,26 @@ class BankAccount
     @interest_rate = rate
   end
 
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def withdraw(amount)
+    @balance -= amount
+  end
+
+  def gain_interest
+    @balance *= 1.00 + interest_rate
+  end
 end
 
 acc1 = BankAccount.new
 acc1.balance = 10200
-acc1.interest_rate = 0.07
+puts "The amount before deposit is #{acc1.balance}"
+acc1.interest_rate = 0.015
+acc1.deposit(500)
+puts "The amount after deposit is #{acc1.balance}"
+acc1.withdraw(1000)
+puts "The balance after withdrawal is #{acc1.balance}"
+acc1.gain_interest
+puts "The balance after gaining interest is #{acc1.balance}"
